e32db15ce23a8bd6d40ef56f57332164a253afc9
huyenpt1001 <huyenpt1001@gmail.com>
2019-01-08 23:44:43 +0700

    BTVN B3

---
+++ web3/homework/app.js
@@ -0,0 +1,3 @@
+var content = document.getElementById("content").addEventListener("input",function(){
+    var check = document.getElementById("count").innerText = 200 - document.getElementById("content").value.length;
+});
